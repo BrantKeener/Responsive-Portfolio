3b5317114e778a6f52c49e897429a83bf1b122e7
Brant Keener <brant_keener@yahoo.com>
2019-02-01 22:00:24 -0700

    Add technology

--- assets/javascript/portfolio.js
+++ assets/javascript/portfolio.js
@@ -1,1 +1,32 @@
+// Change sequelize on hover
 
+// document.addEventListener('DOMContentLoaded', function () {
+//     const seqDisplay = document.getElementById('sequelize');
+//     let displayWord = false;
+//     seqDisplay.addEventListener.onmouseover = () => {
+//         console.log("hi");
+//         displayWord = true;
+//     };
+//     seqDisplay.onmouseleave = () => {
+//         displayWord = false;
+//     };
+//     if(displayWord === true) {
+//         seqDisplay.src = 'assets/images/sequelizeWord.png';
+//     } else {
+//         seqDisplay.src = 'assets/images/sequelize.png';
+//     }
+// });
+
+hoverSequelize = (element) => {
+    if(element === undefined) {
+        unhoverSequelize();
+    } else {
+        element.src = "assets/images/sequelizeWord.png";
+        setTimeout(hoverSequelize, 1000);
+    };
+};
+
+unhoverSequelize = () => {
+    element = document.getElementById('sequelize');
+    element.src = "assets/images/sequelize.png";
+};
